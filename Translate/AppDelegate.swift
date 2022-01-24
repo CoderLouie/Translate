45958de8fc44b26d0bc5a02048924f7a99bf67c9
liyang <18616562401@163.com>
2022-01-24 10:14:06 +0800

    Initial Commit

---
+++ Translate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Translate
+//
+//  Created by 李阳 on 2022/1/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Translate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Translate
+//
+//  Created by 李阳 on 2022/1/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
